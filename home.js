28fe19453590d1005db9f5a1b390e06e27e2c92e
fw20-0769 <tarakkarmakar1434@gmail.com>
2022-08-28 18:12:04 +0530

    My project

---
+++ home.js
@@ -0,0 +1,34 @@
+let x=document.querySelector("#login")
+
+x.addEventListener("click",run)
+  function run(){
+      window.location.href="login.html"
+  }
+
+  let y=document.querySelector("#getstart")
+
+y.addEventListener("click",run1)
+function run1(){
+    window.location.href="createAcount.html"
+}
+
+document.querySelector("#getstart2").addEventListener("click",function(el){
+
+  window.location.href="createAcount.html"
+  })
+  document.querySelector(".get1").addEventListener("click",function(el){
+
+    window.location.href="createAcount.html"
+    })
+    document.querySelector(".get3").addEventListener("click",function(el){
+
+      window.location.href="createAcount.html"
+      })
+      document.querySelector(".get2").addEventListener("click",function(el){
+
+        window.location.href="createAcount.html"
+        })
+        document.querySelector(".get4").addEventListener("click",function(el){
+
+          window.location.href="createAcount.html"
+          })
